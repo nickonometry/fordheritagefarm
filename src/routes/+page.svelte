3ea597927c684ab7ecd0636f8e6c510f1f9819de
nickonometry <nick.andrews@tylertech.com>
2024-08-13 09:56:32 -0400

    chore: created splash page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,18 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<div class="page-container">
+	<img src="/logo.jpg" />
+	<h1>Website Coming Soon</h1>
+</div>
+
+<style>
+	.page-container {
+		display: grid;
+		height: 100%;
+		width: 100%;
+		gap: 24px;
+		place-content: center;
+	}
+
+	h1 {
+		text-align: center;
+	}
+</style>
